56409553403d26adb53f6d29c648d151273b3563
Mohamad Rizki Dafianto <rizki.dafianto@styletheory.co>
2019-11-28 17:27:43 +0700

    feat: Add unit-test for BookViewModel

--- app/src/main/java/com/bookstoresimple/app/presentation/data/Resource.kt
+++ app/src/main/java/com/bookstoresimple/app/presentation/data/Resource.kt
@@ -5,17 +5,17 @@ open class Resource<out T> constructor(
     val data: T?,
     val throwable: Throwable?
 ) {
+    companion object {
+        fun <T> loading(): Resource<T> {
+            return Resource(ResourceState.LOADING, null, null)
+        }
 
-    fun <T> loading(): Resource<T> {
-        return Resource(ResourceState.LOADING, null, null)
+        fun <T> success(data: T): Resource<T> {
+            return Resource(ResourceState.SUCCESS, data, null)
+        }
+
+        fun <T> error(throwable: Throwable?): Resource<T> {
+            return Resource(ResourceState.ERROR, null, throwable)
+        }
     }
-
-    fun <T> success(data: T): Resource<T> {
-        return Resource(ResourceState.SUCCESS, data, null)
-    }
-
-    fun <T> error(throwable: Throwable?): Resource<T> {
-        return Resource(ResourceState.ERROR, null, throwable)
-    }
-
 }
