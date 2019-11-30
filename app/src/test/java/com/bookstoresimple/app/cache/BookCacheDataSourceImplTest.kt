9280f9f2cbf6d889f7248a3d21ba6dce172b28f8
Mohamad Rizki Dafianto <mohamad.rizki.dafianto@gmail.com>
2019-11-30 12:11:46 +0700

    feat: Updatee unit-test cacheDataSource

--- app/src/test/java/com/bookstoresimple/app/cache/BookCacheDataSourceImplTest.kt
+++ app/src/test/java/com/bookstoresimple/app/cache/BookCacheDataSourceImplTest.kt
@@ -4,6 +4,8 @@ import androidx.room.Room
 import com.bookstoresimple.app.TestDataFactory
 import com.bookstoresimple.app.cache.db.BookDatabase
 import com.bookstoresimple.app.cache.preference.BookPreference
+import com.nhaarman.mockito_kotlin.whenever
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -39,7 +41,7 @@ class BookCacheDataSourceImplTest {
     }
 
     @Test
-    fun clearBookList() {
+    fun clearBookList_withComplete() {
 
         val testObserver = bookCacheImpl.clearBookList().test()
 
@@ -47,7 +49,7 @@ class BookCacheDataSourceImplTest {
     }
 
     @Test
-    fun saveBookList() {
+    fun saveBookList_withComplete() {
 
         val mockBookList = TestDataFactory.makeBookList()
 
@@ -57,10 +59,94 @@ class BookCacheDataSourceImplTest {
     }
 
     @Test
-    fun getBookList() {
+    fun saveBookList_withData() {
+
+        val bookCount = 10
+        val mockBookList = TestDataFactory.makeBookList(bookCount)
+
+        bookCacheImpl.saveBookList(mockBookList).test()
+
+        val numberOfRows = mockBookDatabase.bookDao().getBookList().size
+        assertEquals(bookCount, numberOfRows)
+    }
+
+    @Test
+    fun getBookList_withComplete() {
 
         val testObserver = bookCacheImpl.getBookList().test()
 
         testObserver.assertComplete()
+    }
+
+    @Test
+    fun getBookList_withData() {
+
+        val mockBookList = TestDataFactory.makeBookList()
+
+        bookCacheImpl.saveBookList(mockBookList).test()
+
+        val testObserver = bookCacheImpl.getBookList().test()
+
+        testObserver.assertComplete()
+        testObserver.assertValue(mockBookList)
+    }
+
+    @Test
+    fun isCached_withTrue() {
+
+        val isCached = true
+        val mockBookList = TestDataFactory.makeBookList()
+
+        bookCacheImpl.saveBookList(mockBookList).test()
+
+        val testObserver = bookCacheImpl.isCached().test()
+
+        testObserver.assertValue(isCached)
+    }
+
+    @Test
+    fun isCached_withFalse() {
+
+        val isCached = false
+
+        val testObserver = bookCacheImpl.isCached().test()
+
+        testObserver.assertValue(isCached)
+    }
+
+    @Test
+    fun setLastCacheTime() {
+
+        val currentTime = System.currentTimeMillis()
+
+        bookCacheImpl.setLastCacheTime(currentTime)
+
+        assertEquals(mockBookPreference.lastCacheTime, currentTime)
+    }
+
+    @Test
+    fun isExpired_withFalse() {
+
+        val expirationTime = (60 * 10 * 1000).toLong()
+        val currentTime = System.currentTimeMillis() + expirationTime
+
+        bookCacheImpl.setLastCacheTime(currentTime)
+
+        val isExpired = bookCacheImpl.isExpired()
+
+        assertEquals(isExpired, false)
+    }
+
+    @Test
+    fun isExpired_withTrue() {
+
+        val expirationTime = (120 * 10 * 1000).toLong()
+        val currentTime = System.currentTimeMillis() - expirationTime
+
+        bookCacheImpl.setLastCacheTime(currentTime)
+
+        val isExpired = bookCacheImpl.isExpired()
+
+        assertEquals(isExpired, true)
     }
 }
